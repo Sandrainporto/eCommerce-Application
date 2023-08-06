3277c42de40f8ca8f0fd3dd8277f99a96c232e84
vladklen <Vlad6097944@gmail.com>
2023-08-06 11:29:02 +0300

    refactor: husky testt

--- ecommerce-application/wepback.config.js
+++ ecommerce-application/wepback.config.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const fs = require("fs");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
 
 const htmlFile = /^([-_\d\w]+).html$/i;
-const srcPath = path.resolve(__dirname, 'src');
+const srcPath = path.resolve(__dirname, "src");
 
 const devServer = (isDev) =>
   !isDev
@@ -15,7 +15,7 @@ const devServer = (isDev) =>
     : {
         devServer: {
           open: true,
-          port: 'auto',
+          port: "auto",
           static: {
             directory: srcPath,
             watch: true,
@@ -24,7 +24,7 @@ const devServer = (isDev) =>
       };
 
 const getRelative = (absolutePath) => path.relative(srcPath, absolutePath);
-const makePath = (relativePath) => './' + relativePath.replace(/\\+/g, '/');
+const makePath = (relativePath) => "./" + relativePath.replace(/\\+/g, "/");
 
 const getPages = (dir, n) => {
   const dirContent = fs.readdirSync(dir);
@@ -36,17 +36,37 @@ const getPages = (dir, n) => {
         name: `p${(n += i)}`,
         dir: getRelative(dir),
         html: makePath(getRelative(path.join(dir, f))),
-        script: dirContent.find((f) => new RegExp(`^${name}\.[tj]s$`, 'i').test(f)),
-        style: dirContent.find((f) => new RegExp(`^${name}\.s(c|a)ss$`, 'i').test(f)),
+        script: dirContent.find((f) =>
+          new RegExp(`^${name}\.[tj]s$`, "i").test(f),
+        ),
+        style: dirContent.find((f) =>
+          new RegExp(`^${name}\.s(c|a)ss$`, "i").test(f),
+        ),
       });
       return res;
     }, [])
-    .concat(dirContent.filter((f) => fs.lstatSync(path.resolve(dir, f)).isDirectory()).reduce((res, f) => [...res, ...getPages(path.resolve(dir, f), n + 1)], []));
+    .concat(
+      dirContent
+        .filter((f) => fs.lstatSync(path.resolve(dir, f)).isDirectory())
+        .reduce(
+          (res, f) => [...res, ...getPages(path.resolve(dir, f), n + 1)],
+          [],
+        ),
+    );
 
   return pages;
 };
 
-const getEntryPoints = (pages) => pages.reduce((entry, { name, dir, script, style }) => Object.assign(entry, script ? { [name]: makePath(path.join(dir, script)) } : {}, style ? { [`${name}-styles`]: makePath(path.join(dir, style)) } : {}), {});
+const getEntryPoints = (pages) =>
+  pages.reduce(
+    (entry, { name, dir, script, style }) =>
+      Object.assign(
+        entry,
+        script ? { [name]: makePath(path.join(dir, script)) } : {},
+        style ? { [`${name}-styles`]: makePath(path.join(dir, style)) } : {},
+      ),
+    {},
+  );
 
 const getHtmlPlugins = (pages) =>
   pages.map(
@@ -54,54 +74,63 @@ const getHtmlPlugins = (pages) =>
       new HtmlWebpackPlugin({
         template: html,
         filename: html,
-        chunks: [script ? name : null, style ? `${name}-styles` : null].filter((c) => !!c),
-      })
+        chunks: [script ? name : null, style ? `${name}-styles` : null].filter(
+          (c) => !!c,
+        ),
+      }),
   );
 
 module.exports = ({ development }) => {
   const pages = getPages(srcPath, 1);
   return {
-    mode: development ? 'development' : 'production',
-    devtool: development ? 'inline-source-map' : false,
+    mode: development ? "development" : "production",
+    devtool: development ? "inline-source-map" : false,
     entry: getEntryPoints(pages),
     context: srcPath,
     output: {
-      filename: 'js/[name].[contenthash].js',
-      path: path.resolve(__dirname, 'dist'),
-      assetModuleFilename: '[file]',
+      filename: "js/[name].[contenthash].js",
+      path: path.resolve(__dirname, "dist"),
+      assetModuleFilename: "[file]",
     },
-    target: ['web', 'es6'],
+    target: ["web", "es6"],
     module: {
       rules: [
         {
           test: /\.[tj]s$/,
-          use: 'ts-loader',
+          use: "ts-loader",
           exclude: /node_modules/,
         },
         {
           test: /\.(?:ico|gif|png|jpg|jpeg|svg|webp)$/i,
-          type: 'asset/resource',
+          type: "asset/resource",
         },
         {
           test: /\.(?:mp3|wav|ogg|mp4)$/i,
-          type: 'asset/resource',
+          type: "asset/resource",
         },
         {
           test: /\.(woff(2)?|eot|ttf|otf)$/i,
-          type: 'asset/resource',
+          type: "asset/resource",
         },
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.s[ac]ss$/i,
-          use: [{ loader: MiniCssExtractPlugin.loader, options: { publicPath: '../' } }, 'css-loader', 'sass-loader'],
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: { publicPath: "../" },
+            },
+            "css-loader",
+            "sass-loader",
+          ],
         },
       ],
     },
     plugins: [
-      new MiniCssExtractPlugin({ filename: 'css/[name].[contenthash].css' }),
+      new MiniCssExtractPlugin({ filename: "css/[name].[contenthash].css" }),
       ...getHtmlPlugins(pages),
       new CopyPlugin({
         patterns: [
@@ -117,7 +146,7 @@ module.exports = ({ development }) => {
       new RemoveEmptyScriptsPlugin(),
     ],
     resolve: {
-      extensions: ['.js', '.ts'],
+      extensions: [".js", ".ts"],
     },
     ...devServer(development),
   };
