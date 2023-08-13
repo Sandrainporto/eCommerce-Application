2e668d242e05d42335db892b3ef27be110cd6697
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-08-14 00:42:27 +0300

    feat: add slider

---
+++ src/components/Slider/sliderView.ts
@@ -0,0 +1,70 @@
+import './slider.scss';
+import { createElement } from '../../utils/elementCreator';
+import { LeftBtn, RightBtn, SlideParam, Slider, SliderContainer } from './sliderTypes';
+
+function createSlider(root: HTMLElement): HTMLElement {
+  const slider = createElement(Slider, root);
+
+  const numberOfSlides = 4;
+
+  for (let i = 1; i <= numberOfSlides; i += 1) {
+    const slide = createElement(SlideParam, slider) as HTMLImageElement;
+    slide.src = `../../assets/slider/banner${i}.png`;
+    slide.alt = 'slider-image';
+  }
+
+  return slider;
+}
+function updateSlider(slides: HTMLImageElement[], slideIndex: number): void {
+  slides.forEach((slide: HTMLImageElement, index: number) => {
+    const theSlide = slide;
+    if (index === slideIndex) {
+      theSlide.style.display = 'block';
+    } else {
+      theSlide.style.display = 'none';
+    }
+  });
+}
+
+function animateSlider(
+  slider: HTMLElement,
+  leftBtn: HTMLElement,
+  rightBtn: HTMLElement,
+  sliderContainer: HTMLElement,
+): void {
+  const slides: HTMLImageElement[] = Array.from(slider.querySelectorAll('img'));
+  const slideCount: number = slides.length;
+  let slideIndex = 0;
+
+  updateSlider(slides, slideIndex);
+
+  function showNextSlide(): void {
+    slideIndex = (slideIndex + 1) % slideCount;
+    updateSlider(slides, slideIndex);
+  }
+
+  let interval = setInterval(showNextSlide, 3000);
+
+  leftBtn.addEventListener('click', () => {
+    slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+    updateSlider(slides, slideIndex);
+  });
+  rightBtn.addEventListener('click', showNextSlide);
+
+  sliderContainer.addEventListener('mouseover', () => {
+    clearInterval(interval);
+  });
+  sliderContainer.addEventListener('mouseout', () => {
+    interval = setInterval(showNextSlide, 3000);
+  });
+  updateSlider(slides, slideIndex);
+}
+
+export function showSlider(root: HTMLElement): HTMLElement {
+  const sliderContainer = createElement(SliderContainer, root);
+  const leftBtn = createElement(LeftBtn, sliderContainer);
+  const slider = createSlider(sliderContainer);
+  const rightBtn = createElement(RightBtn, sliderContainer);
+  animateSlider(slider, leftBtn, rightBtn, sliderContainer);
+  return sliderContainer;
+}
--- src/components/mainPageContent/mainPageContent.ts
+++ src/components/mainPageContent/mainPageContent.ts
@@ -2,9 +2,11 @@ import './mainPageContent.scss';
 import { ContentMainPageContainer, ContentMainPageParam } from '../../types/types';
 import { createElement } from '../../utils/elementCreator';
 import showCategories from '../Categories/categoriesView';
+import { showSlider } from '../Slider/sliderView';
 
 export default function showContentMainPage(root: HTMLElement): void {
   const content = createElement(ContentMainPageParam, root);
   const contentContainer = createElement(ContentMainPageContainer, content);
+  showSlider(contentContainer);
   showCategories(contentContainer);
 }
