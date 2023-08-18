4775571db9ff124fc5395c42e7b585d90a910f89
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-08-18 16:06:25 +0300

    feat: add 404 page ui

--- src/pages/error/error.ts
+++ src/pages/error/error.ts
@@ -1,12 +1,35 @@
 import './error.scss';
 import showFooter from '../../components/Footer/footerView';
 import showHeader from '../../components/Header/headerView';
-import { MainPageParam } from '../../types/types';
+import {
+  ContentErrorPageParam,
+  MoonImgBlockParam,
+  ErrorPageParam,
+  WitchImgBlockParam,
+  ErrorCodeBlockParam,
+  ErrorDescriptionParam,
+} from './types';
 import { createElement } from '../../utils/elementCreator';
+import moonImg from '../../assets/errorPageImg/moon.png';
+import witchImg from '../../assets/errorPageImg/witch-shadow.png';
 
 export default function showErrorPage(root: HTMLElement): void {
   showHeader(root);
-  const errorPage = createElement(MainPageParam, root);
-  errorPage.innerHTML = '404 Page not found';
+  const errorPage = createElement(ErrorPageParam, root);
+  const errorPageContent = createElement(ContentErrorPageParam, errorPage);
+
+  const moonImageBlock = createElement(MoonImgBlockParam, errorPageContent);
+  moonImageBlock.innerHTML = `<img src=${moonImg} alt="moon-image">`;
+
+  const witchImageBlock = createElement(WitchImgBlockParam, errorPageContent);
+  witchImageBlock.innerHTML = `<img src=${witchImg} alt="moon-image">`;
+
+  const errorMessageCode = createElement(ErrorCodeBlockParam, errorPageContent);
+  errorMessageCode.innerText = '404';
+
+  const errorDescription = createElement(ErrorDescriptionParam, errorPageContent);
+  errorDescription.innerHTML = `<p>Oops, the requested URL was not found.</p> 
+  <a href="#"><span>Go to main page </span></a> `;
+
   showFooter(root);
 }
