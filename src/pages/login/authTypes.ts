8ea58adb8da8b50dde4d3783b1edefc86bac3d34
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-08-21 16:00:06 +0300

    feat: add fields to regForm

--- src/pages/login/authTypes.ts
+++ src/pages/login/authTypes.ts
@@ -97,6 +97,116 @@ export enum UserLNameLabel {
   classNames = 'user_lname_label',
   innerText = 'Last name',
 }
+export enum UserLBirthlInput {
+  elemTag = 'input',
+  type = 'date',
+  classNames = 'new-user_lbirth',
+  id = 'user-lbirth',
+  required = '',
+}
+export enum UserLBirthLabel {
+  elemTag = 'label',
+  for = 'user-lbirth',
+  classNames = 'user_lbirth_label',
+  innerText = 'Date of Birth',
+}
+
+export enum UserAddressBlock {
+  elemTag = 'div',
+  classNames = 'user-address_block',
+  innerText = 'Address',
+}
+export enum AddresslInputCheckbox {
+  elemTag = 'input',
+  type = 'checkbox',
+  classNames = 'new-user_laddress-checkbox',
+  id = 'user-laddress-checkbox',
+  required = '',
+}
+export enum AddressLabelCheckbox {
+  elemTag = 'label',
+  for = 'user-laddress-checkbox',
+  classNames = 'user_laddress_label-checkbox',
+  innerText = 'Set as default address',
+}
+export enum CountrySelectLabel {
+  elemTag = 'label',
+  for = 'country-select',
+  innerText = 'Choose the country',
+}
+export enum CountrySelectBox {
+  elemTag = 'select',
+  classNames = 'new-user_laddress-country',
+  required = '',
+  id = 'country-select',
+}
+export enum CountryOptionUSA {
+  elemTag = 'option',
+  classNames = 'user_laddress_label-checkbox',
+  value = 'Canada',
+  innerText = 'USA',
+}
+export enum CountryOptionBelarus {
+  elemTag = 'option',
+  classNames = 'user_laddress_label-checkbox',
+  value = 'Balarus',
+  innerText = 'Belarus',
+}
+
+export enum UserLTownlInput {
+  elemTag = 'input',
+  type = 'text',
+  classNames = 'new-user_ltown',
+  id = 'user-ltown',
+  required = '',
+}
+export enum UserLTownLabel {
+  elemTag = 'label',
+  for = 'user-ltown',
+  classNames = 'user_ltown_label',
+  innerText = 'Town',
+}
+export enum UserLStreetlInput {
+  elemTag = 'input',
+  type = 'text',
+  classNames = 'new-user_lstreet',
+  id = 'user-lstreet',
+  required = '',
+}
+export enum UserLStreetLabel {
+  elemTag = 'label',
+  for = 'user-lstreet',
+  classNames = 'user_lstreet_label',
+  innerText = 'Street',
+}
+
+export enum UserLPostcodelInput {
+  elemTag = 'input',
+  type = 'text',
+  classNames = 'new-user_lpostcode',
+  id = 'user-lpostcode',
+  required = '',
+}
+export enum UserLPostcodeLabel {
+  elemTag = 'label',
+  for = 'user-lpostcode',
+  classNames = 'user_lpostcode_label',
+  innerText = 'Postcode',
+}
+
+export enum DefAddresslInputCheckbox {
+  elemTag = 'input',
+  type = 'checkbox',
+  classNames = 'new-user_ldefaddress-checkbox',
+  id = 'user-ldefaddress-checkbox',
+  required = '',
+}
+export enum DefAddressLabelCheckbox {
+  elemTag = 'label',
+  for = 'user-ldefaddress-checkbox',
+  classNames = 'user_ldefaddress_label-checkbox',
+  innerText = 'Set as address for shipping and billing ',
+}
 export enum SubmitAuthBtn {
   elemTag = 'a',
   classNames = 'form_btn',
