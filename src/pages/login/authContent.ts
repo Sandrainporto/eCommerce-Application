869e0b394f285a7d5513bb9c7b441943ee5d61b8
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-08-24 12:21:29 +0300

    feat: add billing fields update

--- src/pages/login/authContent.ts
+++ src/pages/login/authContent.ts
@@ -36,6 +36,7 @@ import {
   CountrySelectLabel,
   DefAddresslInputCheckbox,
   DefAddressLabelCheckbox,
+  ContainerName,
 } from './authTypes';
 import { ElementParams, Callback } from '../../types/types';
 import { checkForm } from './formValidation';
@@ -55,30 +56,60 @@ function createFormInput(
   return inputBlock;
 }
 
+function addAddressFields(root: HTMLElement, innerText: string, className: string): HTMLElement {
+  const addressContainer = createElement(UserAddressBlock, root, (e: Event) => checkForm(e));
+  addressContainer.className = className;
+
+  const containerName = createElement(ContainerName, addressContainer);
+  containerName.innerText = innerText;
+
+  createElement(CountrySelectLabel, addressContainer, (e: Event) => checkForm(e));
+
+  const userAddress = createElement(CountrySelectBox, addressContainer, (e: Event) => checkForm(e));
+  createElement(CountryOptionUSA, userAddress, (e: Event) => checkForm(e));
+  createElement(CountryOptionBelarus, userAddress, (e: Event) => checkForm(e));
+  createFormInput(InputBlock, UserLTownLabel, UserLTownlInput, addressContainer, (e: Event) => checkForm(e));
+  createFormInput(InputBlock, UserLStreetLabel, UserLStreetlInput, addressContainer, (e: Event) => checkForm(e));
+  createFormInput(InputBlock, UserLPostcodeLabel, UserLPostcodelInput, addressContainer, (e: Event) => checkForm(e));
+
+  return addressContainer;
+}
+function addBillingFields(): void {
+  const saveBillingChbox = document.querySelector('.new-user_ldefaddress-checkbox');
+  if (saveBillingChbox instanceof HTMLInputElement) {
+    saveBillingChbox.addEventListener('change', () => {
+      const billingBlock = document.querySelector('.user-billing_block');
+      if (billingBlock) {
+        billingBlock.remove();
+      }
+      if (saveBillingChbox.checked === false) {
+        const addressContainer = document.querySelector('.user-address_block') as HTMLElement;
+        addAddressFields(addressContainer, 'Billing Address', 'user-billing_block');
+      }
+    });
+  }
+}
+
 function addFormContent(root: HTMLElement, id: string): HTMLElement {
   const formContent = createElement(FormContent, root);
   if (id === 'registration') {
     const infoContainer = createElement(UserInfoBlock, formContent);
     createFormInput(InputBlock, UserFNameLabel, UserFNamelInput, infoContainer, (e: Event) => checkForm(e));
     createFormInput(InputBlock, UserLNameLabel, UserLNamelInput, infoContainer, (e: Event) => checkForm(e));
+
     createFormInput(InputBlock, UserLBirthLabel, UserLBirthlInput, formContent, (e: Event) => checkForm(e));
-    const addressContainer = createElement(UserAddressBlock, formContent, (e: Event) => checkForm(e));
+
+    const addressContainer = addAddressFields(formContent, 'Address', 'user-address_block');
 
     createFormInput(InputBlock, AddresslInputCheckbox, AddressLabelCheckbox, addressContainer, (e: Event) =>
       checkForm(e),
     );
-
-    createElement(CountrySelectLabel, addressContainer, (e: Event) => checkForm(e));
-    const userAddress = createElement(CountrySelectBox, addressContainer, (e: Event) => checkForm(e));
-    createElement(CountryOptionUSA, userAddress, (e: Event) => checkForm(e));
-    createElement(CountryOptionBelarus, userAddress, (e: Event) => checkForm(e));
-    createFormInput(InputBlock, UserLTownLabel, UserLTownlInput, addressContainer, (e: Event) => checkForm(e));
-    createFormInput(InputBlock, UserLStreetLabel, UserLStreetlInput, addressContainer, (e: Event) => checkForm(e));
-    createFormInput(InputBlock, UserLPostcodeLabel, UserLPostcodelInput, addressContainer, (e: Event) => checkForm(e));
     createFormInput(InputBlock, DefAddresslInputCheckbox, DefAddressLabelCheckbox, addressContainer, (e: Event) =>
       checkForm(e),
     );
+    addBillingFields();
   }
+
   createFormInput(InputBlock, LoginEmailLabel, LoginEmailInput, formContent, (e: Event) => checkForm(e));
 
   createFormInput(InputBlock, LoginPasLabel, LoginPaslInput, formContent, (e: Event) => checkForm(e));
