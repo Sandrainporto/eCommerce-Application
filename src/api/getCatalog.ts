2db3983ffc93827b7bf2e9b5f20d1c360bfd7084
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-08-28 12:58:50 +0300

    feat: add getCatalog request

---
+++ src/api/getCatalog.ts
@@ -0,0 +1,29 @@
+import { Category } from '@commercetools/platform-sdk';
+import createCategoriesCard from '../components/Categories/categoryCard';
+import { apiRoot } from './createClient';
+import { createSublinks } from '../components/Navigaition/navigationView';
+
+export const getCategoriesList = (container: HTMLElement) => {
+  return apiRoot
+    .categories()
+    .get({
+      queryArgs: {
+        expand: ['parent'],
+      },
+    })
+    .execute()
+    .then(({ body }) => {
+      if (body.results.length === 0) {
+        container.innerHTML = `No categories found`;
+      } else {
+        const categoriesData: Category[] = body.results;
+        const catalogeLink: HTMLLinkElement | null = document.querySelector('#catalog');
+        if (catalogeLink) {
+          createSublinks(categoriesData, catalogeLink);
+        }
+
+        createCategoriesCard(container, categoriesData);
+      }
+    })
+    .catch(console.error);
+};
