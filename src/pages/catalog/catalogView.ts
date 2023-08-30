04f1b289590831840d796044f773c13587b62f55
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-08-31 00:28:28 +0300

    fix: add data to catalog

--- src/pages/catalog/catalogView.ts
+++ src/pages/catalog/catalogView.ts
@@ -1,14 +1,17 @@
+import { Category } from '@commercetools/platform-sdk';
 import createCategoriesCard from '../../components/Categories/categoryCard';
-import showFooter from '../../components/Footer/footerView';
-import showHeader from '../../components/Header/headerView';
 import { createElement } from '../../utils/elementCreator';
 import { ContentPageContainer } from '../error/types';
 import { CatalogListParam, CatalogPageParam } from './types';
 
-export default async function showCatalogPage(root: HTMLElement): Promise<void> {
+let categoryData: void | Category[];
+
+export default async function showCatalogPage(root: HTMLElement, data: void | Category[]): Promise<void> {
+  categoryData = data;
+
   const catalogPage = createElement(CatalogPageParam, root);
   const pageContent = createElement(ContentPageContainer, catalogPage);
   const catalogList = createElement(CatalogListParam, pageContent);
 
-  createCategoriesCard(catalogList);
+  createCategoriesCard(catalogList, categoryData);
 }
