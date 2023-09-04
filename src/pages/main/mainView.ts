54e283ebc8cb2bf21131374edd09bceeaebfca22
vladklen <Vlad6097944@gmail.com>
2023-09-05 01:26:07 +0300

    fix: router fix ver 0.3

--- src/pages/main/mainView.ts
+++ src/pages/main/mainView.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import { Category } from '@commercetools/platform-sdk';
 import './main.scss';
 import showHeader from '../../components/Header/headerView';
@@ -13,43 +14,73 @@ let mainWrapper: HTMLElement;
 
 export default function showMainPage(currentPage: string, key?: string): void {
   const url = currentPage.split('/').filter((el) => el.length !== 0);
-  let activePage = Routes[currentPage] || Routes['404'];
   let id = '';
-
-  if (url.length === 0) {
+  let activePage;
+  if (url.length === 0 || !currentPage) {
     activePage = Routes[''];
   } else if (url.length === 1) {
     activePage = Routes[url[0]] || Routes['404'];
   } else if (url.length === 2) {
-    Data.map((el) => {
+    Data.forEach((el) => {
       if (el.category.key === url[1]) {
         id = el.category.id;
-      } else {
+        activePage = Routes.products;
+        return;
+      }
+      if (!id) {
         activePage = Routes['404'];
       }
-      return undefined;
     });
-  } else if (url.length > 2) {
+  } else if (url.length === 3 && !key) {
     Data.forEach((el) => {
       el.subcategory.forEach((elem) => {
-        if (elem.key === url[2]) id = elem.id;
+        if (elem.key === url[2]) {
+          id = elem.id;
+          activePage = Routes.products;
+        }
+        if (!id) {
+          activePage = Routes['404'];
+        }
       });
     });
-  }
-  console.log(activePage);
-  console.log(url);
-  console.log(currentPage);
-  mainWrapper.innerHTML = '';
-  showBreadcrumb(mainWrapper);
-  if (id) {
-    activePage = Routes.products;
-    activePage(mainWrapper, id);
-  } else if (key && url.length === 3) {
+  } else if (key) {
     activePage = Routes.details;
-    activePage(mainWrapper, key);
-  } else if (!id && activePage === Routes['404']) {
-    activePage(mainWrapper, categoryData);
-  } else activePage(mainWrapper, categoryData);
+  }
+  // if (url.length === 0) {
+  //   activePage = Routes[currentPage];
+  // } else if (url.length === 1) {
+  //   activePage = Routes[url[0]] || Routes['404'];
+  // } else if (url.length === 2) {
+
+  // } else if (url.length > 2) {
+  // Data.forEach((el) => {
+  //   el.subcategory.forEach((elem) => {
+  //     if (elem.key === url[2]) id = elem.id;
+  //   });
+  // });
+  // }
+  // console.log(activePage);
+  // console.log(url);
+  // console.log(currentPage);
+  // if (id) {
+  //   activePage = Routes.products;
+  //   activePage(mainWrapper, id);
+  // } else if (key && url.length === 3) {
+  // activePage = Routes.details;
+  //   activePage(mainWrapper, key);
+  // } else if (!id && activePage === Routes['404']) {
+  //   activePage(mainWrapper, categoryData);
+  // } else {
+  //   console.log(123123);
+  //   activePage(mainWrapper, categoryData);
+  // }
+  mainWrapper.innerHTML = '';
+  if (activePage === Routes['404']) {
+    const path = window.location.host;
+    window.location.href = `http://${path}/404`;
+  }
+  showBreadcrumb(mainWrapper);
+  activePage(mainWrapper, key || id || categoryData);
 }
 
 export const setData = (wrapper: HTMLElement, data: DataType[]): void => {
