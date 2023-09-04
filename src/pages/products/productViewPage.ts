af32b1ac060be8eddfe05d341eff29929a8d6df4
vladklen <Vlad6097944@gmail.com>
2023-09-04 20:22:21 +0300

    fix: eslint errors fixed, refactor ProductViewPage

--- src/pages/products/productViewPage.ts
+++ src/pages/products/productViewPage.ts
@@ -22,14 +22,52 @@ let SearchParameter = '';
 let ContentRoot: HTMLElement;
 let CurrentId: string;
 
+const SortParams = {
+  0: 'name.en-us asc',
+  1: 'name.en-us desc',
+  2: 'price desc',
+  3: 'price asc',
+};
+
+const createCard = (root: HTMLElement, product: ProductProjection): void => {
+  const currentUrl = window.location.href;
+  const productCard = createElement(ProductCard, root);
+  const productIconBox = createElement(ProductImageBox, productCard);
+  const productIcon = createElement(ProductImage, productIconBox) as HTMLImageElement;
+  const productImagesData: Image[] | undefined = product.masterVariant.images;
+  if (productImagesData) {
+    const mainImage = productImagesData[0];
+    productIcon.src = mainImage.url;
+  }
+  const productTitle = createElement(ProductName, productCard);
+  productTitle.innerText = product.name['en-US'];
+  const productDescription = createElement(ProductDescription, productCard);
+  if (product.description) {
+    productDescription.innerText = product.description['en-US'];
+  } else {
+    productDescription.innerText = '';
+  }
+  const productPricesData: Price[] | undefined = product.masterVariant.prices;
+  productPricesData?.forEach((prices) => {
+    const productPrice = createElement(ProductPrice, productCard);
+    productPrice.innerText = `${prices.value.centAmount / 100} ${prices.value.currencyCode}`;
+  });
+  const productLink = createElement(ProductCardLink, productCard) as HTMLAnchorElement;
+  productLink.href = `${currentUrl}/${product.key?.toLowerCase()}-card`;
+  productLink.id = `${product.key?.toLowerCase()}`;
+};
+
+export async function showCards(id: string, productsList: HTMLElement): Promise<void> {
+  const productData: ProductProjection[] = await getProductsList(id, [SortParams[SortParameter]]);
+  productData.forEach((product) => {
+    createCard(productsList, product);
+  });
+}
+
 const updatePage = (): void => {
-  ContentRoot.innerHTML =``
-  const productsList =  document.querySelector('.products__list') as HTMLElement
-  console.log(SortParameter, SearchParameter);
-showCards(CurrentId, productsList)
-
-
-  
+  ContentRoot.innerHTML = ``;
+  const productsList = document.querySelector('.products__list') as HTMLElement;
+  showCards(CurrentId, productsList);
 };
 
 const SortCallBack = (value: string): void => {
@@ -42,7 +80,6 @@ const SearchCallBack = (value: string): void => {
   updatePage();
 };
 
-// eslint-disable-next-line max-lines-per-function
 export default async function showProductsPage(root: HTMLElement, id: string): Promise<void> {
   CurrentId = id;
   const productsPage = createElement(ProductsPageParam, root);
@@ -53,51 +90,5 @@ export default async function showProductsPage(root: HTMLElement, id: string): P
   productsList.id = id;
   ContentRoot = productsList;
 
-
-  showCards(id, productsList)
-}
-export async function showCards(id:string, productsList:HTMLElement):Promise<void>{
-  const currentUrl = window.location.href;
-console.log(SortParameter, SearchParameter)
-let productData : ProductProjection[] =await getProductsList(id, ["name.en-us asc"]);
-if(SortParameter ==0){
- productData = await getProductsList(id, ["name.en-us asc"]);
-}else if(SortParameter ==1){
-  productData = await getProductsList(id, ["name.en-us desc"]);
-}
-else if(SortParameter ==2){
-  productData = await getProductsList(id, ["price desc"]);
-}
-else if(SortParameter ==3){
-  productData = await getProductsList(id, ["price asc"]);
-}
-
-  productData.forEach((product) => {
-    const productCard = createElement(ProductCard, productsList);
-    const productIconBox = createElement(ProductImageBox, productCard);
-    const productIcon = createElement(ProductImage, productIconBox) as HTMLImageElement;
-    const productImagesData: Image[] | undefined = product.masterVariant.images;
-    if (productImagesData) {
-      const mainImage = productImagesData[0];
-      productIcon.src = mainImage.url;
-    }
-    const productTitle = createElement(ProductName, productCard);
-    productTitle.innerText = product.name['en-US'];
-    const productDescription = createElement(ProductDescription, productCard);
-    if (product.description) {
-      productDescription.innerText = product.description['en-US'];
-    } else {
-      productDescription.innerText = '';
-    }
-    const productPricesData: Price[] | undefined = product.masterVariant.prices;
-    productPricesData?.forEach((prices) => {
-      const productPrice = createElement(ProductPrice, productCard);
-      productPrice.innerText = `${prices.value.centAmount / 100} ${prices.value.currencyCode}`;
-    });
-    const productLink = createElement(ProductCardLink, productCard) as HTMLAnchorElement;
-    productLink.href = `${currentUrl}/${product.key?.toLowerCase()}-card`;
-    productLink.id = `${product.key?.toLowerCase()}`;
-  });
-
-
+  showCards(id, productsList);
 }
