8c1967f64cdf0829568e9100f9218bf0ebb24302
vladklen <Vlad6097944@gmail.com>
2023-09-05 21:56:18 +0300

    filter: color filter added

--- src/api/getProducts.ts
+++ src/api/getProducts.ts
@@ -5,15 +5,17 @@ export async function getProductsList(
   categoryId: string,
   sortparam: string[],
   searchText: string,
+  filter: string[],
   fuzzyLevel: number | undefined,
 ): Promise<ProductProjection[]> {
+  const filterColors = filter.length !== 0 ? `variants.attributes.Color:"${filter.join()}"` : '';
   const { body } = await apiRoot
     .productProjections()
     .search()
-
     .get({
       queryArgs: {
-        filter: `categories.id:"${categoryId}"`,
+        // filter: `categories.id:"${categoryId}"`,
+        'filter.query': [`categories.id:"${categoryId}"`, `${filterColors}`],
         sort: sortparam,
         'text.en-us': searchText,
         fuzzy: true,
