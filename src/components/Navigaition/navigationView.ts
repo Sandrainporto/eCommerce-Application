84807ff88d55d8637a6e32a1e416c0abc15010f8
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-09-12 09:07:27 +0300

    fix: add profile btn while reloading if logged

--- src/components/Navigaition/navigationView.ts
+++ src/components/Navigaition/navigationView.ts
@@ -127,6 +127,14 @@ async function createNavigationLinks(root: HTMLElement): Promise<void> {
   }
   const profileLink: HTMLLinkElement | null = document.querySelector('#profile');
   profileLink?.classList.add('hide');
+  const loggedUserName = localStorage.getItem('userName');
+  const registratedUserName = localStorage.getItem('reg-customer-name')?.slice(1, -2);
+
+  if (loggedUserName || registratedUserName) {
+    profileLink?.classList.remove('hide');
+
+  }
+
 }
 
 export default function showNavigation(root: HTMLElement, data: DataType[]): HTMLElement {
