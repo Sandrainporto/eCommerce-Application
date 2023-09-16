dbb19ccb2b1d33a04d0b8465055d7ef4b454298d
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-09-16 23:02:51 +0300

    feat: add basic structure

--- src/pages/aboutUs/showAboutUsPage.ts
+++ src/pages/aboutUs/showAboutUsPage.ts
@@ -3,21 +3,65 @@ import './aboutUs.scss';
 import {
   AboutUsBlock,
   AboutUsPage,
-  CODERS,
   CodersInfo,
   ImageBox,
   InfoContainer,
+  PersonalBio,
   PersonalContent,
   PersonalImage,
   PersonalInfo,
+  PersonalLink,
   PersonalName,
+  PersonalRole,
+  PersonalSprintContribution,
   PersonalSprints,
   TabsBtns,
   TabsContent,
   TabsParam,
+  CodersTypes,
+  BtnParam,
+  Content,
+  Btns,
 } from './types';
+function addPersonalInfo(root, chosenCoder): HTMLElement {
+  const personalInfo = createElement(PersonalInfo, root);
+  if (chosenCoder) {
+    let { name, photo, role, bio, gitHub } = chosenCoder;
 
-function openContent(coderName: string, event?):void {
+    const imageContainer = createElement(ImageBox, personalInfo);
+    const personalImage = createElement(PersonalImage, imageContainer) as HTMLImageElement;
+    personalImage.src = photo;
+    personalImage.alt = `${name}-image`;
+
+    const allInfo = createElement(InfoContainer, personalInfo);
+
+    const personalName = createElement(PersonalName, allInfo);
+    personalName.innerText = name;
+    const personalRole = createElement(PersonalRole, allInfo);
+    personalRole.innerText = role;
+    const personalBio = createElement(PersonalBio, allInfo);
+    personalBio.innerText = bio;
+    const personalLink = createElement(PersonalLink, allInfo);
+    personalLink.innerText = gitHub;
+  }
+  return personalInfo;
+}
+
+function openContent(coderName: string, personalContainer: HTMLElement, event?): void {
+  console.log(coderName);
+  personalContainer.id = coderName;
+  const previousInfo = document.querySelector(`.${PersonalInfo.classNames}`)?.remove();
+  const previousSprints = document.querySelector(`.${PersonalSprints.classNames}`)?.remove();
+
+  let chosenCoder: CodersTypes | undefined = CodersInfo.find((coder) => coder.name === personalContainer.id);
+  addPersonalInfo(personalContainer, chosenCoder);
+  showPersonalContribution(personalContainer, chosenCoder);
+
+  const firstSprint = document.querySelectorAll(`.${TabsBtns.classNames}`)[0];
+  firstSprint.setAttribute('defaultState', 'defaultOpen');
+  openTab(firstSprint.innerHTML);
+}
+function openTab(chosenSprint: string, event?): void {
   let tabContent: HTMLElement[] = Array.from(
     document.getElementsByClassName(TabsContent.classNames) as HTMLCollectionOf<HTMLElement>,
   );
@@ -34,51 +78,57 @@ function openContent(coderName: string, event?):void {
     link.className.replace(' active', '');
   });
 
-  (document.getElementById(coderName) as HTMLElement).style.display = 'block';
-  if(event){
-  event.currentTarget.className += ' active';
+  (document.getElementById(chosenSprint) as HTMLElement).style.display = 'block';
+  if (event) {
+    event.currentTarget.className += ' active';
+  }
+}
+
+function showPersonalContribution(root: HTMLElement, chosenCoder: CodersTypes | undefined) {
+  const personalSprints = createElement(PersonalSprints, root);
+  const tabSprintsContainer = createElement(TabsParam, personalSprints);
+  console.log(chosenCoder);
+  if (chosenCoder) {
+    let { sprints } = chosenCoder;
+
+    Object.keys(sprints).forEach((key, index) => {
+      const tabLinkSprints = createElement(TabsBtns, tabSprintsContainer);
+      tabLinkSprints.innerText = key;
+      const tabSprintsContent = createElement(TabsContent, personalSprints);
+      tabSprintsContent.id = key;
+      const sprintContribution = createElement(PersonalSprintContribution, tabSprintsContent);
+      sprintContribution.innerText = sprints[Object.keys(sprints)[index]];
+
+      tabLinkSprints.addEventListener('click', (e: MouseEvent) => {
+        const chosenSprint = (e.target as HTMLElement).innerText;
+        console.log(chosenSprint);
+        openTab(chosenSprint, e);
+      });
+    });
   }
 }
 
 export function showAboutUsPage(root: HTMLElement): HTMLElement {
   const page = createElement(AboutUsPage, root);
   const aboutUs = createElement(AboutUsBlock, page);
-  const tabContainer = createElement(TabsParam, aboutUs);
-  CodersInfo.map(({name, photo, id}) => {
-    const tabLink = createElement(TabsBtns, tabContainer);
+  const btnsContainer = createElement(BtnParam, aboutUs);
+  const content = createElement(Content, aboutUs);
+  const personalContainer = createElement(PersonalContent, content);
+
+  CodersInfo.map(({ name, attribute, sprints }) => {
+    const tabLink = createElement(Btns, btnsContainer);
     tabLink.innerText = name;
-
-
-    const tabContent = createElement(TabsContent, aboutUs);
-    tabContent.id = name;
-    if(id){
-      tabLink.id = id;
-      openContent(tabContent.id)
+    tabLink.id = `${name}`;
+    if (attribute) {
+      tabLink.setAttribute('defaultState', attribute);
+      openContent(tabLink.innerText, personalContainer);
     }
-
-
-    const personalContainer = createElement(PersonalContent, tabContent);
-    const personalInfo = createElement(PersonalInfo, personalContainer);
-
-    const imageContainer = createElement(ImageBox, personalInfo);
-    const personalImage = createElement(PersonalImage, imageContainer)as HTMLImageElement;
-      personalImage.src = photo;
-      personalImage.alt = `${name}-image`
-
-const allInfo= createElement(InfoContainer, personalInfo)
-
-    const personalName = createElement(PersonalName, allInfo);
-    personalName.innerText = name;
-
-    const personalSprints = createElement(PersonalSprints, personalContainer);
 
     tabLink.addEventListener('click', (e: MouseEvent) => {
       const coderName = (e.target as HTMLElement).innerText;
-      openContent(coderName, e);
+      openContent(coderName, personalContainer, e);
     });
   });
 
   return page;
 }
-
-// .addEventListener("click", function (event) { event.currentTarget.className += " active"; });
--- src/pages/aboutUs/types.ts
+++ src/pages/aboutUs/types.ts
@@ -12,6 +12,19 @@ export enum AboutUsBlock {
   classNames = 'about-us__block',
 }
 
+export enum BtnParam {
+  elemTag = 'div',
+  classNames = 'tab-btn',
+}
+export enum Btns {
+  elemTag = 'button',
+  classNames = 'tab-btn__button',
+}
+export enum Content {
+  elemTag = 'div',
+  classNames = 'tab-btn__content',
+}
+
 export enum TabsParam {
   elemTag = 'div',
   classNames = 'tab',
@@ -20,49 +33,85 @@ export enum TabsBtns {
   elemTag = 'button',
   classNames = 'tabLink',
 }
-export const CODERS = ['Vladislav Klenovski', 'Alexandra Barmin', 'Artem Hlopov'];
 
 export enum TabsContent {
   elemTag = 'div',
   classNames = 'tabcontent',
 }
 
-
 export enum PersonalContent {
   elemTag = 'div',
   classNames = 'personal__content',
+  id = '1',
 }
-
-
 
 export enum PersonalInfo {
   elemTag = 'div',
   classNames = 'personal__info',
 }
-export const CodersInfo =[{
-  name:'Vladislav Klenovski',
-  photo: 'https://vladklen.github.io/rsschool-cv/assets/img/profile-img.png',
-  role:'role',
-  bio: 'Passionate junior front-end developer with a desire to learn and grow in a collaborative team environment. Skilled in HTML, CSS, JavaScript and react. Since 2014 i\'am working as a civil engineer.Work in the construction industry taught me how to solve the most unexpected problems. I am able to work in a team and also have skills of leadership and team management.',
-  gitHub:'https://github.com/vladklen',
-  id: 'defaultOpen'
+
+export interface CodersTypes {
+  id: string;
+  name: string;
+  photo: string;
+  role: string;
+  bio: string;
+  gitHub: string;
+  attribute?: string;
+  sprints: {
+    sprint1: string;
+    sprint2: string;
+    sprint3: string;
+    sprint4: string;
+  };
+}
+
+export const CodersInfo:CodersTypes[] = [
+  {
+    id: '1',
+    name: 'Vladislav Klenovski',
+    photo: 'https://vladklen.github.io/rsschool-cv/assets/img/profile-img.png',
+    role: 'role',
+    bio: "Passionate junior front-end developer with a desire to learn and grow in a collaborative team environment. Skilled in HTML, CSS, JavaScript and react. Since 2014 i'am working as a civil engineer.Work in the construction industry taught me how to solve the most unexpected problems. I am able to work in a team and also have skills of leadership and team management.",
+    gitHub: 'https://github.com/vladklen',
+    attribute: 'defaultOpen',
+    sprints: {
+      sprint1: '111',
+      sprint2: '222',
+      sprint3: '333',
+      sprint4: '4444',
+    },
   },
   {
-    name:'Alexandra Barmin',
+    id: '2',
+    name: 'Alexandra Barmin',
     photo: 'https://avatars.githubusercontent.com/u/90930428?v=4',
-    role:'role',
-  bio: 'bio',
-  gitHub:'https://github.com/Sandrainporto'
+    role: 'role',
+    bio: 'bio',
+    gitHub: 'https://github.com/Sandrainporto',
+    sprints: {
+      sprint1: '121',
+      sprint2: '232',
+      sprint3: '343',
+      sprint4: '4844',
     },
-    {
-      name:'Artem Hlopov',
-      photo: 'https://artemhlopov.github.io/rsschool-cv/assets/img/photo_2022-12-15_22-54-17.jpg',
-      role:'role',
-  bio: 'I\'m tired of my job and I want to change my profession',
-  gitHub:'https://github.com/ArtemHlopov'
-      }]
-  
-  
+  },
+  {
+    id: '3',
+    name: 'Artem Hlopov',
+    photo: 'https://artemhlopov.github.io/rsschool-cv/assets/img/photo_2022-12-15_22-54-17.jpg',
+    role: 'role',
+    bio: "I'm tired of my job and I want to change my profession",
+    gitHub: 'https://github.com/ArtemHlopov',
+    sprints: {
+      sprint1: '77',
+      sprint2: '2862',
+      sprint3: '365',
+      sprint4: '9644',
+    },
+  },
+];
+
 export enum ImageBox {
   elemTag = 'div',
   classNames = 'personal__image-box',
@@ -72,23 +121,35 @@ export enum PersonalImage {
   classNames = 'personal__image',
 }
 
-
 export enum InfoContainer {
   elemTag = 'div',
   classNames = 'info-container',
 }
 
-
-
 export enum PersonalName {
   elemTag = 'h3',
   classNames = 'personal__name',
 }
+export enum PersonalRole {
+  elemTag = 'h4',
+  classNames = 'personal__role',
+}
+export enum PersonalBio {
+  elemTag = 'p',
+  classNames = 'personal__bio',
+}
 
-
-
+export enum PersonalLink {
+  elemTag = 'a',
+  classNames = 'personal__link',
+}
 
 export enum PersonalSprints {
   elemTag = 'div',
   classNames = 'personal__sprints',
+}
+
+export enum PersonalSprintContribution {
+  elemTag = 'p',
+  classNames = 'personal__contribution',
 }
