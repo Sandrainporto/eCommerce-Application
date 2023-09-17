5bc3d42a275dc91c34c0757b83d3933fcb6300e9
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-09-18 01:10:07 +0300

    feat: add about us page

--- src/pages/aboutUs/showAboutUsPage.ts
+++ src/pages/aboutUs/showAboutUsPage.ts
@@ -1,5 +1,6 @@
-import { createElement } from '../../utils/elementCreator';
 import './aboutUs.scss';
+import gitHubIcon from './../../assets/icons/github-icon.svg';
+import { createElement } from '../../utils/elementCreator';
 import {
   AboutUsBlock,
   AboutUsPage,
@@ -22,11 +23,36 @@ import {
   BtnParam,
   Content,
   Btns,
+  BioParagraph,
+  LinkImg,
 } from './types';
+
+function openTab(chosenSprint: string, event?): void {
+  const tabContent: HTMLElement[] = Array.from(
+    document.getElementsByClassName(TabsContent.classNames) as HTMLCollectionOf<HTMLElement>,
+  );
+
+  tabContent.forEach((content) => {
+    content.style.display = 'none';
+  });
+
+  const tabLinks: HTMLElement[] = Array.from(
+    document.getElementsByClassName(TabsBtns.classNames) as HTMLCollectionOf<HTMLElement>,
+  );
+
+  tabLinks.forEach((link) => {
+    link.className.replace(' active', '');
+  });
+
+  (document.getElementById(chosenSprint) as HTMLElement).style.display = 'block';
+  if (event) {
+    event.currentTarget.className += ' active';
+  }
+}
 function addPersonalInfo(root, chosenCoder): HTMLElement {
   const personalInfo = createElement(PersonalInfo, root);
   if (chosenCoder) {
-    let { name, photo, role, bio, gitHub } = chosenCoder;
+    const { name, photo, role, bio, gitHub } = chosenCoder;
 
     const imageContainer = createElement(ImageBox, personalInfo);
     const personalImage = createElement(PersonalImage, imageContainer) as HTMLImageElement;
@@ -40,64 +66,42 @@ function addPersonalInfo(root, chosenCoder): HTMLElement {
     const personalRole = createElement(PersonalRole, allInfo);
     personalRole.innerText = role;
     const personalBio = createElement(PersonalBio, allInfo);
-    personalBio.innerText = bio;
-    const personalLink = createElement(PersonalLink, allInfo);
-    personalLink.innerText = gitHub;
+    
+  bio.forEach((item) => {
+      console.log(item)
+      const paragraph = createElement(BioParagraph, personalBio);
+      paragraph.innerText = item
+    });
+    const personalLink = createElement(PersonalLink, allInfo) as HTMLAnchorElement;
+    personalLink.href =  gitHub;
+    const linkImage = createElement(LinkImg, personalLink)as HTMLImageElement;
+    linkImage.src =gitHubIcon ;
   }
   return personalInfo;
 }
-
-function openContent(coderName: string, personalContainer: HTMLElement, event?): void {
-  console.log(coderName);
-  personalContainer.id = coderName;
-  const previousInfo = document.querySelector(`.${PersonalInfo.classNames}`)?.remove();
-  const previousSprints = document.querySelector(`.${PersonalSprints.classNames}`)?.remove();
-
-  let chosenCoder: CodersTypes | undefined = CodersInfo.find((coder) => coder.name === personalContainer.id);
-  addPersonalInfo(personalContainer, chosenCoder);
-  showPersonalContribution(personalContainer, chosenCoder);
-
-  const firstSprint = document.querySelectorAll(`.${TabsBtns.classNames}`)[0];
-  firstSprint.setAttribute('defaultState', 'defaultOpen');
-  openTab(firstSprint.innerHTML);
-}
-function openTab(chosenSprint: string, event?): void {
-  let tabContent: HTMLElement[] = Array.from(
-    document.getElementsByClassName(TabsContent.classNames) as HTMLCollectionOf<HTMLElement>,
-  );
-
-  tabContent.forEach((content) => {
-    content.style.display = 'none';
-  });
-
-  let tabLinks: HTMLElement[] = Array.from(
-    document.getElementsByClassName(TabsBtns.classNames) as HTMLCollectionOf<HTMLElement>,
-  );
-
-  tabLinks.forEach((link) => {
-    link.className.replace(' active', '');
-  });
-
-  (document.getElementById(chosenSprint) as HTMLElement).style.display = 'block';
-  if (event) {
-    event.currentTarget.className += ' active';
-  }
-}
-
 function showPersonalContribution(root: HTMLElement, chosenCoder: CodersTypes | undefined) {
   const personalSprints = createElement(PersonalSprints, root);
   const tabSprintsContainer = createElement(TabsParam, personalSprints);
   console.log(chosenCoder);
   if (chosenCoder) {
-    let { sprints } = chosenCoder;
+    const { sprints } = chosenCoder;
 
     Object.keys(sprints).forEach((key, index) => {
       const tabLinkSprints = createElement(TabsBtns, tabSprintsContainer);
       tabLinkSprints.innerText = key;
       const tabSprintsContent = createElement(TabsContent, personalSprints);
       tabSprintsContent.id = key;
-      const sprintContribution = createElement(PersonalSprintContribution, tabSprintsContent);
-      sprintContribution.innerText = sprints[Object.keys(sprints)[index]];
+      console.log(sprints[Object.keys(sprints)[index]])
+      sprints[Object.keys(sprints)[index]].forEach(line=>{
+        const sprintContribution = createElement(PersonalSprintContribution, tabSprintsContent);
+        if(!line.includes('●')){
+          console.log(line)
+          sprintContribution.classList.add('colored')
+
+        }
+        sprintContribution.innerText = line;
+
+      })
 
       tabLinkSprints.addEventListener('click', (e: MouseEvent) => {
         const chosenSprint = (e.target as HTMLElement).innerText;
@@ -107,6 +111,20 @@ function showPersonalContribution(root: HTMLElement, chosenCoder: CodersTypes | 
     });
   }
 }
+function openContent(coderName: string, personalContainer: HTMLElement, event?): void {
+  console.log(coderName);
+  personalContainer.id = coderName;
+  const previousInfo = document.querySelector(`.${PersonalInfo.classNames}`)?.remove();
+  const previousSprints = document.querySelector(`.${PersonalSprints.classNames}`)?.remove();
+
+  const chosenCoder: CodersTypes | undefined = CodersInfo.find((coder) => coder.name === personalContainer.id);
+  addPersonalInfo(personalContainer, chosenCoder);
+  showPersonalContribution(personalContainer, chosenCoder);
+
+  const firstSprint = document.querySelectorAll(`.${TabsBtns.classNames}`)[0];
+  firstSprint.setAttribute('defaultState', 'defaultOpen');
+  openTab(firstSprint.innerHTML);
+}
 
 export function showAboutUsPage(root: HTMLElement): HTMLElement {
   const page = createElement(AboutUsPage, root);
@@ -115,7 +133,7 @@ export function showAboutUsPage(root: HTMLElement): HTMLElement {
   const content = createElement(Content, aboutUs);
   const personalContainer = createElement(PersonalContent, content);
 
-  CodersInfo.map(({ name, attribute, sprints }) => {
+  CodersInfo.map(({ name, attribute }) => {
     const tabLink = createElement(Btns, btnsContainer);
     tabLink.innerText = name;
     tabLink.id = `${name}`;
--- src/pages/aboutUs/types.ts
+++ src/pages/aboutUs/types.ts
@@ -31,7 +31,7 @@ export enum TabsParam {
 }
 export enum TabsBtns {
   elemTag = 'button',
-  classNames = 'tabLink',
+  classNames = 'tab-link',
 }
 
 export enum TabsContent {
@@ -51,63 +51,62 @@ export enum PersonalInfo {
 }
 
 export interface CodersTypes {
-  id: string;
   name: string;
   photo: string;
   role: string;
-  bio: string;
+  bio: string[];
   gitHub: string;
   attribute?: string;
   sprints: {
-    sprint1: string;
-    sprint2: string;
-    sprint3: string;
-    sprint4: string;
+    Sprint1: string[];
+    Sprint2: string[];
+    Sprint3: string[];
+    Sprint4: string[];
   };
 }
 
-export const CodersInfo:CodersTypes[] = [
+export const CodersInfo: CodersTypes[] = [
   {
-    id: '1',
     name: 'Vladislav Klenovski',
     photo: 'https://vladklen.github.io/rsschool-cv/assets/img/profile-img.png',
-    role: 'role',
-    bio: "Passionate junior front-end developer with a desire to learn and grow in a collaborative team environment. Skilled in HTML, CSS, JavaScript and react. Since 2014 i'am working as a civil engineer.Work in the construction industry taught me how to solve the most unexpected problems. I am able to work in a team and also have skills of leadership and team management.",
+    role: 'Team Lead',
+    bio: ['Passionate junior front-end developer with a desire to learn and grow in a collaborative team environment. Skilled in HTML, CSS, JavaScript and react.', 
+    'Since 2014 i am working as a civil engineer.Work in the construction industry taught me how to solve the most unexpected problems.', 
+    'I am able to work in a team and also have skills of leadership and team management.'],
     gitHub: 'https://github.com/vladklen',
     attribute: 'defaultOpen',
     sprints: {
-      sprint1: '111',
-      sprint2: '222',
-      sprint3: '333',
-      sprint4: '4444',
+      Sprint1: ['Development of Environment Configuration', '● adding Webpack, Typescript, ESlint,Prettier, Jest, Husky to the project and the necessary dependencies', 'Development of Scripts', '● writing necessary scripts' ],
+      Sprint2: ['Routing Implementation', '● implementation of routing for navigation between login, registration, main and 404 pages', 'Registration Page Implementation', '● Finalization of client-side validation for all required fields in the registration form'],
+      Sprint3: ['Routing Implementation', '● implementation of routing for navigation between Catalog page, Product detail page, User Profile page and the other pages', 'Catalog Page Implementation', '● creation of Product Filtering, Sorting, and Searching fields', 'Code refactoring, bug fixing'],
+      Sprint4: ['Routing Implementation', '● implementation of routing for navigation to theBasket Page, About Us page and the other pages', 'Catalog Page Enhancements', '● implementation and creation of the pagination', 'Bug Fixing'],
     },
   },
   {
-    id: '2',
     name: 'Alexandra Barmin',
-    photo: 'https://avatars.githubusercontent.com/u/90930428?v=4',
-    role: 'role',
-    bio: 'bio',
+    photo: 'https://media.githubusercontent.com/avatars/32935565?orig=1&token=AVVXZ7A2CK2SY3KIX2GO5TLFA6DRQ',
+    role: 'Designer, ideological inspirer',
+    bio: ['Very interested in creation of uniq and stylish apps and webpages that are beautiful outside and intelligent under the hood.',
+    'My current goal and interest is: to focus on TS + React + Redux more deeply, probably to learn UI/Ux design in the future to stronger my work results and to make them look more interesting.'],
     gitHub: 'https://github.com/Sandrainporto',
     sprints: {
-      sprint1: '121',
-      sprint2: '232',
-      sprint3: '343',
-      sprint4: '4844',
+      Sprint1: ['Repository Setup', '● creation of private repo with `main` and `develop` branches', '● add gitignore, pull_request_template files', '● add initial folder structure', 'Task Board Setup', '● creation and making changes through the project period', 'Comprehensive README'],
+      Sprint2: ['Main Page Creation', '● creation of centralise navigation, slider, columns, adding main categories', '404 Page Creation', '● creation of animation for the 404 page', 'Evaluation Criteria for Header', '● adding show/hide functionality for log in/ log out / registration buttons', 'Registration Page Implementation', '● adding date of birth, address and billing address fields', 'Writing of unit tests'],
+      Sprint3: ['Catalog Page Implementation', '● creation of the products in the commercetools', '● displaying interactive products card list according to the commercetools data', '● creation of the categoty navigation (subcategories menu, breadcrumbs )', 'Detailed Product Page Implementation','● displaying interactive product card eith the necessary data and fields according to the commercetools data', '● adding image slider to each card', 'Evaluation Criteria for Header',],
+      Sprint4: ['About Us Page Implementation', '● creation of the About Us page with the provided by team members information','Catalog Page Enhancements', '● adding and fixing code lines according the requirements', 'Detailed Product Page Implementation','● displaying interactive "Add to Cart" button that changes it\'s view according to the product position in the cart', 'Evaluation Criteria for Header '],
     },
   },
   {
-    id: '3',
     name: 'Artem Hlopov',
     photo: 'https://artemhlopov.github.io/rsschool-cv/assets/img/photo_2022-12-15_22-54-17.jpg',
-    role: 'role',
-    bio: "I'm tired of my job and I want to change my profession",
+    role: 'Backend communicater',
+    bio: ['I\'m tired of my job and I want to change my profession.', 'Graduated from Belarusian National Technical University.', 'Finished cources: Development of web applications with JavaScript, IT-academy; Website development using HTML, CSS and JavaScript, IT-academy'],
     gitHub: 'https://github.com/ArtemHlopov',
     sprints: {
-      sprint1: '77',
-      sprint2: '2862',
-      sprint3: '365',
-      sprint4: '9644',
+      Sprint1: ['CommerceTools Project and API Client Setup', '● creation of the project in CommerceTools, setting up the currencies, languages, and other configurations in the Merchant Center', '● creation of the API client for CommerceTools, enabling access to the platform\'s APIs and managing permissions and scopes required for the project.'],
+      Sprint2: ['Login Page Implementation', '● creation of login page with a client-side validation', '●  integration of the form with an authentication service', '● obtaining of the authentication token securely after a successful login attempt', 'Registration Page Implementation', '● creation of the registration page with a client-side validation', '●  integration of the form with an authentication service', '● redirection of users upon successful account creation and automatic login. '],
+      Sprint3: ['User Profile Page Implementation', '● creation of the User Profile Page with nessesary fields', '● adding ability to change user profile information'],
+      Sprint4: ['Basket Page Implementation', '● creation of the Basket Page with nessesary fields', 'Catalog Page Enhancements', '● displaying items','● modify product quantity','● adding, removing products','● calculation of hte total cost','● ability to clear the shopping cart','● ability to apply promocode'],
     },
   },
 ];
@@ -135,13 +134,21 @@ export enum PersonalRole {
   classNames = 'personal__role',
 }
 export enum PersonalBio {
-  elemTag = 'p',
+  elemTag = 'div',
   classNames = 'personal__bio',
+}
+export enum   BioParagraph {
+  elemTag = 'p',
+  classNames = 'bio__paragrahp',
 }
 
 export enum PersonalLink {
   elemTag = 'a',
   classNames = 'personal__link',
+}
+export enum LinkImg {
+  elemTag = 'img',
+  classNames = 'personal__link_img',
 }
 
 export enum PersonalSprints {
