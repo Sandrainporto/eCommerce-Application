0642177e4e8ae8622a20507812eb3cce368860cf
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-09-18 01:13:55 +0300

    fix: fix minor bugs in adaptivity

--- src/components/mainContent/mainContent.ts
+++ src/components/mainContent/mainContent.ts
@@ -13,10 +13,9 @@ import { showAboutUsPage } from '../../pages/aboutUs/showAboutUsPage';
 let categoryData: void | Category[];
 
 const getPageContent = async (root: HTMLElement): Promise<void> => {
-  showAboutUsPage(root);
-  // showSlider(root);
-  // showPageInfo(root);
-  // createCategoriesCard(root, categoryData);
+  showSlider(root);
+  showPageInfo(root);
+  createCategoriesCard(root, categoryData);
 };
 
 export default function showMainContent(root: HTMLElement, data: void | Category[]): void {
